daa2860cafe90bad40692b44527d7de6ea5c67f5
G.cheng <G.cheng1108@foxmail.com>
2020-03-04 19:59:25 +0800

    第二次上传项目

---
+++ homework2/js/index.js
@@ -0,0 +1,16 @@
+var proName = "";
+var proVersion = "";
+var proAuth = "";
+function pageOnload(){
+    initPage(proName?proName:"第1周作业",proVersion?proVersion:"1.0",proVersion?proVersion:"chenggang");
+}
+
+function initPage(proName,proVersion,proAuth){
+    $("projectName").innerHTML = proName;
+    $("projectVersion").innerHTML = proVersion;
+    $("projectAuth").innerHTML = proAuth;
+}
+
+function $(id){
+    return document.getElementById(id);
+}
